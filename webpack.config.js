1ff1f63e7ad8c422641a888e3ab4e0d6c87375ed
Stepan <steprusskihasd@gmail.com>
2024-08-27 03:32:18 +0300

    Finished css layout

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,7 @@ module.exports = {
   devServer: {
     port: 3000,
     hot: isDev,
+    watchFiles: ['src/index.html'], // Здесь изменения
   },
   plugins: [
     isDev ? new ESLintPlugin() : null,
